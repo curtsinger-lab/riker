6aed8269f3705aa795be0d40f9ae3284e079c2ac
Daniel W. Barowy <dbarowy@cs.williams.edu>
2019-03-26 17:18:13 -0400

    remove empty-string args

--- parse_strace.py
+++ parse_strace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import re
 import sys
-from typing import List, Optional
+from typing import List, Dict, Optional
 
 class Event:
     def __init__(self, pid: int, name: str, args: List[str], retval: Optional[int]) -> None :
@@ -11,7 +11,7 @@ class Event:
         self.retval: Optional[int] = retval
 
     def __str__(self) -> str :
-        return "pid: {}, function: {}, args: {}, retval: {}".format(self.pid, self.name, self.args, self.retval)
+        return "pid: {}, event: {}, args: {}, retval: {}".format(self.pid, self.name, self.args, self.retval)
 
 # complain
 def usage():
@@ -21,7 +21,10 @@ def usage():
 # parse argument lists
 def argparse(args: str) -> List[str] :
     xs: List[str] = args.split(',')
+    # remove leading whitespace
     xs = [x.lstrip() for x in xs]
+    # remove empty strings
+    xs = list(filter(None, xs))
     return xs
 
 # parse return values
@@ -30,15 +33,16 @@ def rvparse(rv: str) -> Optional[int] :
         return None
     else:
         return int(rv)
-
+    
 # top-level parsing function
 def parse(lines: List[str]) -> List[Event] :
     a: List[Event] = []
+    starts: Dict[int,Event] = {}
     # case 1: most lines match this regex
     r = '(?P<pid>[0-9]+)\s+(?P<name>[a-z0-9_]+)\((?P<args>.*)\)\s+=\s+(?P<retval>-?[0-9?]+)'
     # case 2: other lines start and are finished at later points
     # case 2 start:
-    rstart = '(?P<pid>[0-9]+)\s+(?P<name>[a-z0-9_]+)\((?P<args>.*)unfinished'
+    rstart = '(?P<pid>[0-9]+)\s+(?P<name>[a-z0-9_]+)\((?P<args>.*)<unfinished'
     # case 2 end:
     rend = '(?P<pid>[0-9]+)\s+<\.\.\.\s+(?P<name>[a-z0-9_]+)\s+resumed>\s+(?P<args>.*)\)\s+=\s+(?P<retval>-?[0-9?]+)'
     # case 3: signal
@@ -48,18 +52,14 @@ def parse(lines: List[str]) -> List[Event] :
     for i in range(0, len(lines)):
         line = lines[i]
         m = re.match(r, line)
-
-        pid: int = -999
-        name: str = "DEBUG"
-        args: List[str] = []
-        retval: Optional[int] = -999
         
         # check case 1 
         if m:
             pid    = int(m.group('pid'))
-            name  = m.group('name')        
+            name   = m.group('name')        
             args   = argparse(m.group('args'))
             retval = rvparse(m.group('retval'))
+            a.append(Event(pid, name, args, retval))
         # check case 2
         else:
             # check case 2 start
@@ -69,23 +69,26 @@ def parse(lines: List[str]) -> List[Event] :
                 name = m.group('name')
                 args = argparse(m.group('args'))
                 retval = None
+                starts[pid] = Event(pid, name, args, retval)
             # check case 2 end
             else:
                 m = re.match(rend, line)
                 if m:
                     pid    = int(m.group('pid'))
                     name  = m.group('name')
-                    args   = []
                     retval = rvparse(m.group('retval'))
-                # check case 3
+                    e = starts[pid]
+                    del starts[pid]
+                    a.append(Event(pid, name, e.args, retval))
+                # check case 3: signals
                 else:
                     m = re.match(rsig, line)
                     if m:
                         pid = int(m.group('pid'))
                         name = m.group('name')
                         args = argparse(m.group('args'))
-                        retval = None
-                    # check case 4
+                        a.append(Event(pid, name, args, None))
+                    # check case 4: exit
                     else:
                         m = re.match(rexit, line)
                         if m:
@@ -94,7 +97,7 @@ def parse(lines: List[str]) -> List[Event] :
                         else:                        
                             print("Unable to parse: ", line)
                             sys.exit(1)
-        a.append(Event(pid, name, args, retval))
+        
             
     return a
 
