2299bb3299c374f6204d9d579991c388c1a77597
Charlie Curtsinger <curtsinger@grinnell.edu>
2019-03-26 14:13:25 -0500

    Helpful dev setup messages

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,7 +1,16 @@
 #/bin/bash
 
 echo "Setting up python virtual environment"
-python3 -m venv dev
+if ! python3 -m venv dev; then
+  echo "Failed to set up virtual environment. Is python3-venv installed?"
+  echo 
+  echo "If you prefer, you can install dependencies globally (not recommended):"
+  echo "  pip3 install -r dev-requirements.txt"
+  echo
+  echo "To install venv on Ubuntu/Debian:"
+  echo "  sudo apt-get install python3-venv"
+  exit
+fi
 
 echo "Installing packages for development"
 source dev/bin/activate
@@ -10,6 +19,9 @@ deactivate
 
 echo
 echo "Setup finished."
-echo "Run \`source dev/bin/activate\` to enter virtual environment."
-echo "Run \`deactivate\` to leave virtual environment."
+echo "To enter the virtual environment, run:"
+echo "  source dev/bin/activate"
+echo
+echo "When you are ready to leave the virtual environment run:"
+echo "  deactivate"
 
