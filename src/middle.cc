2eef8f3af3d8ee2967619cc8f6b92d56515c89d6
Alissa Johnson <johnsona5@grinnell.edu>
2019-06-17 15:45:05 -0500

    Broad skeleton of middle.

--- src/middle.cc
+++ src/middle.cc
@@ -7,20 +7,36 @@
 
 #include "middle.h"
 
+
+void Command::add_input(std::string filename) {
+	return;
+}
+
+void Command::add_output(std::string filename) {
+	return;
+}
+
+
 void trace_state::add_dependency(pid_t thread_id, struct file_reference file, enum dependency_type type) {
     fprintf(stderr, "[%d] Dep: ", thread_id);
-    switch (type) {
+	//std::string path = this->processes.find(thread_id)->second.fds.find(file.fd)->second;
+	switch (type) {
+	    //TODO handle file side of things 
     case DEP_READ:
         fprintf(stderr, "read");
-        break;
+        //this->processes.find(thread_id)->second.command.add_input(path);
+	break;
     case DEP_MODIFY:
         fprintf(stderr, "modify");
+        //this->processes.find(thread_id)->second.command.add_output(path);
         break;
     case DEP_CREATE:
         fprintf(stderr, "create");
-        break;
+        // create edge
+	break;
     case DEP_REMOVE:
         fprintf(stderr, "remove");
+        // delete edge
         break;
     }
     if (!file.follow_links) {
@@ -37,6 +53,7 @@ void trace_state::add_dependency(pid_t thread_id, struct file_reference file, en
 
 void trace_state::add_change_cwd(pid_t thread_id, struct file_reference file) {
     fprintf(stderr, "[%d] Change working directory to ", thread_id);
+    //this->processes.find(thread_id)->second.cwd = file.path;
     if (file.fd == AT_FDCWD) {
         fprintf(stderr, "%s\n", file.path);
     } else {
@@ -46,6 +63,7 @@ void trace_state::add_change_cwd(pid_t thread_id, struct file_reference file) {
 
 void trace_state::add_change_root(pid_t thread_id, struct file_reference file) {
     fprintf(stderr, "[%d] Change root to ", thread_id);
+    //this->processes.find(thread_id)->second.root = file.path;
     if (file.fd == AT_FDCWD) {
         fprintf(stderr, "%s\n", file.path);
     } else {
@@ -53,15 +71,24 @@ void trace_state::add_change_root(pid_t thread_id, struct file_reference file) {
     }
 }
 
+
+// get filenames from their open 
 void trace_state::add_open(pid_t thread_id, int fd, struct file_reference file, int access_mode, bool is_rewrite) {
-    fprintf(stderr, "[%d] Open %d -> ", thread_id, fd);
-    if (file.fd == AT_FDCWD) {
+   	//TODO rethink versioning with open 
+	fprintf(stderr, "[%d] Open %d -> ", thread_id, fd);
+	// take into account root and cwd
+	//struct Process cur_proc = this->processes.find(thread_id)->second;
+	//std::string path = cur_proc.root + cur_proc.cwd + file.path;
+    //cur_proc.fds.insert(std::pair<int, std::string>(fd,path));
+	if (file.fd == AT_FDCWD) {
         fprintf(stderr, "%s\n", file.path);
     } else {
         fprintf(stderr, "{%d/}%s\n", file.fd, file.path);
     }
 }
 
+
+// TODO handle these two
 void trace_state::add_pipe(pid_t thread_id, int fds[2]) {
     fprintf(stderr, "[%d] Pipe %d, %d\n", thread_id, fds[0], fds[1]);
 }
@@ -76,14 +103,17 @@ void trace_state::add_mmap(pid_t thread_id, int fd) {
 }
 
 // TODO: Do we need this?
+// close edges?
 void trace_state::add_close(pid_t thread_id, int fd) {
     fprintf(stderr, "[%d] Close %d\n", thread_id, fd);
 }
 
+// create process node
 void trace_state::add_fork(pid_t parent_thread_id, pid_t child_process_id) {
     fprintf(stderr, "[%d] Fork %d\n", parent_thread_id, child_process_id);
 }
 
+// fill in process node
 void trace_state::add_exec(pid_t process_id, char* exe_path) {
     fprintf(stderr, "[%d] Inside exec: %s\n", process_id, exe_path);
     free(exe_path);
@@ -94,6 +124,7 @@ void trace_state::add_exec_argument(pid_t process_id, char* argument, int index)
     free(argument);
 }
 
+// TODO close relevant mmaps
 void trace_state::add_exit(pid_t thread_id) {
     fprintf(stderr, "[%d] Exit\n", thread_id);
 }
