f3c75df366309fa69e0afea8ea28c66c7850d884
Alissa Johnson <johnsona5@grinnell.edu>
2019-06-20 13:54:51 -0500

    Adding basic graph output.

---
+++ src/graph/graph.cc
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+
+#include "graph.h"
+
+void Graph::start_graph(void) {
+    this->graph.open("out.gv");
+    this->graph << "digraph {\n\tgraph [rankdir=LR]\n\tnode [fontname=Courier]\n";
+} 
+
+void Graph::add_node(std::string id, std::string label, std::string attr) {
+   this->graph << "\t\"" + id + "\" [label=\"" + label + "\"" + attr + "]\n";
+}
+
+void Graph::add_edge(std::string id1, std::string id2, std::string attr) {
+    this->graph << "\t\"" + id1 + "\" -> \"" + id2 + "\" [" + attr + "]\n";
+}
+
+void Graph::close_graph(void) {
+    this->graph << "}";
+    this->graph.close();
+}    
