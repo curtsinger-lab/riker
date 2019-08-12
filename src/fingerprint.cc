284f4cba4f6393065f30a80e9dd3249139eee23d
Charlie Curtsinger <curtsinger@grinnell.edu>
2019-08-12 16:41:57 -0500

    Working fingerprints for directory contents. Code cleanup coming next

--- src/fingerprint.cc
+++ src/fingerprint.cc
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <dirent.h>
 
 #include "util.h"
 #include "db.capnp.h"
@@ -40,36 +41,87 @@ void set_fingerprint(db::File::Builder file, bool use_checksum) {
     file.setInode(stat_info.st_ino);
     file.setMode(stat_info.st_mode);
 
-    if (!use_checksum || (stat_info.st_mode & S_IFMT) != S_IFREG) {
-        // Only checksum regular files
+    // Skip checksum when requested
+    if (!use_checksum) {
         file.setFingerprintType(db::FingerprintType::METADATA_ONLY);
         return;
     }
-
-    int file_fd = open(path_string.c_str(), O_RDONLY | O_CLOEXEC);
-    if (file_fd < 0) { // Error opening
-        file.setFingerprintType(db::FingerprintType::METADATA_ONLY);
-        return;
-    }
-
-    auto checksum = file.initChecksum(BLAKE2S_OUTBYTES);
-    blake2sp_state hash_state;
-    blake2sp_init(&hash_state, BLAKE2S_OUTBYTES);
-    char buffer[1 << 13];
-    while (true) {
-        auto bytes_read = read(file_fd, buffer, ARRAY_COUNT(buffer));
-        if (bytes_read > 0) {
-            blake2sp_update(&hash_state, buffer, bytes_read);
-        } else if (bytes_read == 0) { // EOF
-            blake2sp_final(&hash_state, checksum.begin(), checksum.size());
-            file.setFingerprintType(db::FingerprintType::BLAKE2SP);
-            close(file_fd);
-            return;
-        } else { // Error reading
+    
+    // Is this a file or a directory?
+    if ((stat_info.st_mode & S_IFMT) == S_IFDIR) {
+        // Checksum a directory
+        
+        // Get a sorted list of directory entries
+        struct dirent **namelist;
+        int entries = scandir(path_string.c_str(),
+                              &namelist,
+                              [](const struct dirent* e) {
+                                  std::string name(e->d_name);
+                                  if (name == "db.dodo") return 0;
+                                  else return 1;
+                              },
+                              alphasort);
+        
+        // Did reading the directory fail?
+        if (entries == -1) {
+            // Unable to read directory
             file.setFingerprintType(db::FingerprintType::METADATA_ONLY);
-            close(file_fd);
             return;
         }
+        
+        // Start a checksum
+        auto checksum = file.initChecksum(BLAKE2S_OUTBYTES);
+        blake2sp_state hash_state;
+        blake2sp_init(&hash_state, BLAKE2S_OUTBYTES);
+        
+        std::cerr << "Checksummed contents of " << path_string << std::endl;
+        // Add each directory entry into the checksum
+        for(int i=0; i<entries; i++) {
+            std::cerr << "  " << namelist[i]->d_name << std::endl;
+            // Hash the entry name and null terminator to mark boundaries between entries
+            blake2sp_update(&hash_state, namelist[i]->d_name, strlen(namelist[i]->d_name)+1);
+            free(namelist[i]);
+        }
+        
+        // Clean up the name list
+        free(namelist);
+        
+        // Finish the checksum
+        blake2sp_final(&hash_state, checksum.begin(), checksum.size());
+        file.setFingerprintType(db::FingerprintType::BLAKE2SP);
+        return;
+        
+    } else if ((stat_info.st_mode & S_IFMT) == S_IFREG) {
+        // Checksum files contents
+        int file_fd = open(path_string.c_str(), O_RDONLY | O_CLOEXEC);
+        if (file_fd < 0) { // Error opening
+            file.setFingerprintType(db::FingerprintType::METADATA_ONLY);
+            return;
+        }
+
+        auto checksum = file.initChecksum(BLAKE2S_OUTBYTES);
+        blake2sp_state hash_state;
+        blake2sp_init(&hash_state, BLAKE2S_OUTBYTES);
+        char buffer[1 << 13];
+        while (true) {
+            auto bytes_read = read(file_fd, buffer, ARRAY_COUNT(buffer));
+            if (bytes_read > 0) {
+                blake2sp_update(&hash_state, buffer, bytes_read);
+            } else if (bytes_read == 0) { // EOF
+                blake2sp_final(&hash_state, checksum.begin(), checksum.size());
+                file.setFingerprintType(db::FingerprintType::BLAKE2SP);
+                close(file_fd);
+                return;
+            } else { // Error reading
+                file.setFingerprintType(db::FingerprintType::METADATA_ONLY);
+                close(file_fd);
+                return;
+            }
+        }
+    } else {
+        // Don't checksum other types of files
+        file.setFingerprintType(db::FingerprintType::METADATA_ONLY);
+        return;
     }
 }
 
@@ -115,37 +167,97 @@ bool match_fingerprint(db::File::Reader file) {
     if (file.getFingerprintType() == db::FingerprintType::METADATA_ONLY) {
         return false;
     }
+    
+    // Is this a file or a directory?
+    if ((stat_info.st_mode & S_IFMT) == S_IFDIR) {
+        // Checksum a directory
+        
+        // Get a sorted list of directory entries
+        struct dirent **namelist;
+        int entries = scandir(path_string.c_str(),
+                              &namelist,
+                              [](const struct dirent* e) {
+                                  std::string name(e->d_name);
+                                  if (name == "db.dodo") return 0;
+                                  else return 1;
+                              },
+                              alphasort);
+        
+        // Did reading the directory fail?
+        if (entries == -1) {
+            // Unable to read directory
+            return false;
+        }
+        
+        // Start a checksum
+        size_t hash_size = file.getChecksum().size();
+        
+        switch (file.getFingerprintType()) {
+        case db::FingerprintType::BLAKE2SP: {
+            KJ_STACK_ARRAY(kj::byte, checksum, hash_size, hash_size, hash_size);
+            blake2sp_state hash_state;
+            blake2sp_init(&hash_state, hash_size);
+            
+            std::cerr << "Checksummed contents of " << path_string << std::endl;
+            // Add each directory entry into the checksum
+            for(int i=0; i<entries; i++) {
+                std::cerr << "  " << namelist[i]->d_name << std::endl;
+                // Hash the entry name and null terminator to mark boundaries between entries
+                blake2sp_update(&hash_state, namelist[i]->d_name, strlen(namelist[i]->d_name)+1);
+                free(namelist[i]);
+            }
 
-    // Otherwise, avoid false negatives as commonly happen with insubstantial input changes
-    // by checking against a checksum.
-    int file_fd = open(path_string.c_str(), O_RDONLY | O_CLOEXEC);
-    if (file_fd < 0) { // Error opening
-        return false;
-    }
+            // Clean up the name list
+            free(namelist);
 
-    size_t hash_size = file.getChecksum().size();
-    switch (file.getFingerprintType()) {
-    case db::FingerprintType::BLAKE2SP: {
-        KJ_STACK_ARRAY(kj::byte, checksum, hash_size, hash_size, hash_size);
-        blake2sp_state hash_state;
-        blake2sp_init(&hash_state, hash_size);
-        char buffer[1 << 13];
-        while (true) {
-            auto bytes_read = read(file_fd, buffer, ARRAY_COUNT(buffer));
-            if (bytes_read > 0) {
-                blake2sp_update(&hash_state, buffer, bytes_read);
-            } else if (bytes_read == 0) { // EOF
-                blake2sp_final(&hash_state, checksum.begin(), checksum.size());
-                close(file_fd);
-                return checksum.asConst() == file.getChecksum();
-            } else { // Error reading
-                close(file_fd);
+            // Finish the checksum
+            blake2sp_final(&hash_state, checksum.begin(), checksum.size());
+            
+            if (checksum.asConst() == file.getChecksum()) {
+                std::cerr << "Directory matches" << std::endl;
+                return true;
+            } else {
                 return false;
             }
         }
-    }
-    default:
-        close(file_fd);
-        return false;
+        default:
+            return false;
+        }
+        
+    } else if ((stat_info.st_mode & S_IFMT) == S_IFREG) {
+        // Checksum a regular file
+        
+        // Avoid false negatives as commonly happen with insubstantial input changes
+        // by checking against a checksum.
+        int file_fd = open(path_string.c_str(), O_RDONLY | O_CLOEXEC);
+        if (file_fd < 0) { // Error opening
+            return false;
+        }
+
+        size_t hash_size = file.getChecksum().size();
+        switch (file.getFingerprintType()) {
+        case db::FingerprintType::BLAKE2SP: {
+            KJ_STACK_ARRAY(kj::byte, checksum, hash_size, hash_size, hash_size);
+            blake2sp_state hash_state;
+            blake2sp_init(&hash_state, hash_size);
+            char buffer[1 << 13];
+            while (true) {
+                auto bytes_read = read(file_fd, buffer, ARRAY_COUNT(buffer));
+                if (bytes_read > 0) {
+                    blake2sp_update(&hash_state, buffer, bytes_read);
+                } else if (bytes_read == 0) { // EOF
+                    blake2sp_final(&hash_state, checksum.begin(), checksum.size());
+                    close(file_fd);
+                    return checksum.asConst() == file.getChecksum();
+                } else { // Error reading
+                    close(file_fd);
+                    return false;
+                }
+            }
+        }
+        default:
+            close(file_fd);
+            return false;
+        }
     }
 }
