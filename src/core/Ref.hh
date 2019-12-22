9593876c2aa1fe4e1870cca39fe26ff3247eb2d3
Charlie Curtsinger <curtsinger@grinnell.edu>
2019-12-22 11:55:09 -0600

    Comments for Ref

--- src/core/Ref.hh
+++ src/core/Ref.hh
@@ -12,11 +12,14 @@ using std::shared_ptr;
 
 /// A reference to an artifact
 class Ref : public std::enable_shared_from_this<Ref> {
+  /// Only Commands can create Ref instances
   friend class Command;
 
  public:
+  /// Flags that control the resolution of a reference
   struct Flags {
    public:
+    /// Create a Flags instance from the flags parameter to the open syscall
     static Flags fromOpen(int flags) {
       return {.r = (flags & O_RDONLY) == O_RDONLY || (flags & O_RDWR) == O_RDWR,
               .w = (flags & O_WRONLY) == O_WRONLY || (flags & O_RDWR) == O_RDWR,
@@ -26,6 +29,7 @@ class Ref : public std::enable_shared_from_this<Ref> {
               .exclusive = (flags & O_EXCL) == O_EXCL};
     }
 
+    /// Create a Flags instance from the mode and flags parameters to the access syscall
     static Flags fromAccess(int mode, int flags) {
       return {.r = (mode & R_OK) == R_OK,
               .w = (mode & W_OK) == W_OK,
@@ -33,26 +37,44 @@ class Ref : public std::enable_shared_from_this<Ref> {
               .nofollow = (flags & AT_SYMLINK_NOFOLLOW) == AT_SYMLINK_NOFOLLOW};
     }
 
+    /// Create a Flags instance from the flags parameter to the stat syscall
     static Flags fromStat(int flags) { return fromAccess(0, flags); }
 
+    /// Create a Flags instance from the flags parameter to the chown syscall
     static Flags fromChown(int flags) { return fromAccess(0, flags); }
 
+    /// Create a Flags instance from the flags parameter to the chmod syscall
     static Flags fromChmod(int flags) { return fromAccess(0, flags); }
 
+    /// Create a Flags instance from the flags parameter to the unlink syscall
     static Flags fromUnlink(int flags) { return fromAccess(0, flags); }
 
+    /// Print a Flags instance
     friend ostream& operator<<(ostream& o, const Flags& f) {
       o << (f.r ? 'r' : '-') << (f.w ? 'w' : '-') << (f.x ? 'x' : '-');
       if (f.nofollow) o << " nofollow";
       return o;
     }
 
+    /// Is the reference readable?
     bool r = false;
+
+    /// Is the reference writable?
     bool w = false;
+
+    /// Is the reference executable?
     bool x = false;
+
+    /// If the reference refers to a symlink, do we follow it?
     bool nofollow = false;
+
+    /// Does the reference truncate the file when resolved?
     bool truncate = false;
+
+    /// Does the reference create the file when resolved? May be as-needed, or required (see below)
     bool create = false;
+
+    /// Is the reference *required* to create the file when resolved?
     bool exclusive = false;
   };
 
@@ -142,27 +164,14 @@ class Ref : public std::enable_shared_from_this<Ref> {
   /// The command that made this reference
   weak_ptr<Command> _command;
 
-  /// The artifact this reference resolves to. This is optional because some references are
-  /// path-only, and no attempt is made to resolve them. If there is an artifact pointer but it is
-  /// not set, that means the reference failed to resolve.
-  optional<shared_ptr<Artifact>> _artifact = nullopt;
-
-  /// The path used to reach the artifact. If not set, the reference was not established via path.
-  /// This will happen for pipes, but we should know the path for any entity on the filesystem.
-  /// That's true even when a command establishes a reference through a sequence of accesses like
-  /// openat() or fchdir(). The file descriptors those calls use to reference locations are known
-  /// and their paths can be accessed and composed to generate full paths for any reference.
+  /// The artifact this reference resolves to, if it has been resolved.
+  optional<shared_ptr<Artifact>> _artifact;
+  
+  /// The path to the artifact. This will be set for any artifact that has a path, even if it is an
+  /// anonymous reference (e.g. a file opened before an exec call).
   optional<string> _path;
 
-  /// Flags from the open syscall. This captures a few things we care about:
-  ///  is the reference readable?
-  ///  is the reference writable?
-  ///  is the reference closed on an exec call? (O_CLOEXEC)
-  ///  is the reference set up to create the file? (O_CREAT)
-  ///  is the reference required to create the file? (O_EXCL)
-  ///  is the reference opened without following links? (O_NOFOLLOW)
-  /// We need to track these flags because they allow us to determine whether this reference will
-  /// resolve differently on some future run of the command that makes the reference.
+  /// Flags that control the resolution of this reference
   Flags _flags;
 
   /// Is this reference made without naming the path to the artifact?
