a6bf0bc6c510dfbc948594bf13725255018b0964
Daniel Barowy <dbarowy@cs.williams.edu>
2020-09-11 15:48:51 +0000

    Mostly working benchmark.

--- benchmarks/run-benchmarks.sh
+++ benchmarks/run-benchmarks.sh
@@ -3,6 +3,6 @@
 ## RUN
 
 # calc
-docker run --name benchmark-calc -dit dbarowy/benchmark-calc:v1
+docker run --security-opt seccomp=unconfined --name benchmark-calc -dit dbarowy/benchmark-calc:v1
 docker exec benchmark-calc /benchmark/run.sh
 
