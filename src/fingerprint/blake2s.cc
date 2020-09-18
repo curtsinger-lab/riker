d64da7eda12caa00186c9f07a1d0c101889386d8
Charlie Curtsinger <curtsinger@grinnell.edu>
2020-09-17 20:32:20 -0500

    Dropping fingerprint code for now

--- src/fingerprint/blake2s.cc
+++
@@ -1,8 +0,0 @@
-// IMPORTANT NOTE: Keep the feature detection here (SSE vs not) in line with blake2-wrapper.h
-
-#if defined(__SSE__) && defined(__SSE2__)
-// TODO: consider ifuncs and dynamic feature detection
-#include "../deps/blake2/sse/blake2s.c"
-#else
-#include "../deps/blake2/ref/blake2s-ref.c"
-#endif
