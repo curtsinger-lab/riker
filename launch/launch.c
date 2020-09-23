862c0085a47b261e772699c52d0913cffab87a87
Daniel Barowy <dbarowy@cs.williams.edu>
2020-09-23 19:30:02 +0000

    look for all of the makefiles

--- launch/launch.c
+++ launch/launch.c
@@ -1,11 +1,13 @@
 #include <errno.h>
 #include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
 #define RootBuildCommand "Dodofile"
-#define Makefile "Makefile"
+#define MAKEFILES \
+  { "GNUmakefile", "makefile", "Makefile" }
 #define ShellCommand "/bin/sh"
 #define MakeCommand "/usr/bin/make"
 
@@ -21,13 +23,17 @@ int main() {
   } else {
     // The Dodofile is either unreadable or it doesn't exist at all.
     // Is there a readable Makefile available?
-    if (faccessat(AT_FDCWD, Makefile, R_OK, AT_EACCESS) == 0) {
-      execl(MakeCommand, MakeCommand, NULL);
+    const char* makefiles[] = MAKEFILES;
+    for (int i = 0; i < sizeof(makefiles) / sizeof(makefiles[0]); i++) {
+      if (faccessat(AT_FDCWD, makefiles[i], R_OK, AT_EACCESS) == 0) {
+        // a successful call will not return
+        execl(MakeCommand, MakeCommand, NULL);
+      }
     }
   }
 
   // Looks like those did not work
-  fprintf(stderr, "Unable to find either " RootBuildCommand " or " Makefile ".\n");
+  fprintf(stderr, "Unable to find either " RootBuildCommand " or Makefile.\n");
   fprintf(stderr, "  A " RootBuildCommand
                   " file should be present and be either directly"
                   " executable or runnable with " ShellCommand ".\n");
