450099b80df1cc7725d00b6c9f0026d8f4092998
Daniel Barowy <dbarowy@cs.williams.edu>
2020-10-21 16:58:49 +0000

    Dodofile for Linux

---
+++ benchmarks/scripts/getfreshlinux.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget --no-clobber https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.9.1.tar.xz
+tar xf linux-5.9.1.tar.xz
+cp dodo/benchmarks/linux-kernel/.config linux-5.9.1/
+cp dodo/benchmarks/linux-kernel/Dodofile linux-5.9.1/
