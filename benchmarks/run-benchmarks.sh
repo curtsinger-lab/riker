ddf3f09ca99cb94fc5b3ca8300f9e16ca026de61
Daniel W. Barowy <dbarowy@cs.williams.edu>
2020-11-05 15:15:55 -0500

    Remove scripts superseded by run.py script.

--- benchmarks/run-benchmarks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-## RUN
-
-# calc
-docker run --security-opt seccomp=unconfined --name benchmark-calc -dit dbarowy/benchmark-calc:v1
-docker exec benchmark-calc /benchmark/run.sh
-
--- benchmarks/setup-benchmarks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-## note that the Docker context is the dodo repository's root directory ##
-
-## SETUP ##
-
-# calc
-docker build -f calc/Dockerfile -t dbarowy/benchmark-calc:v1 ../
