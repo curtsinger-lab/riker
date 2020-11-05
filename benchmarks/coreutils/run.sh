11446cc9542ee8346065457340f25e79a6db6dd7
Daniel W. Barowy <dbarowy@cs.williams.edu>
2020-11-05 09:32:47 -0500

    coreutils benchmark config

---
+++ benchmarks/coreutils/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+cd $BENCHMARK_ROOT/$BENCHMARK_NAME
+
+# -f is:
+# e     elapsed wall clock time, in seconds
+# S     cpu-seconds of system time
+# U     cpu-seconds of user time
+# P     percent CPU time
+# t     average resident set size in kb
+
+echo "\"wall_s\",\"system_s\",\"user_s\",\"pct_cpu\",\"avg_rss\"" > $TIME_CSV
+/usr/bin/time -f"\"%e\",\"%S\",\"%U\",\"%P\",\"%t\"" --output=$TIME_CSV --append ../../dodo/dodo --stats $TMP_CSV
