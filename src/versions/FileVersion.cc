caeea6014b991a68a3019df32558069e217f0c84
Daniel W. Barowy <dbarowy@cs.williams.edu>
2020-12-17 15:04:45 -0500

    Temporarily rename canCommit to isSaved and make virtual

--- src/versions/FileVersion.cc
+++ src/versions/FileVersion.cc
@@ -13,7 +13,7 @@
 #include "util/log.hh"
 
 // Is this version saved in a way that can be committed?
-bool FileVersion::canCommit() const noexcept {
+bool FileVersion::isSaved() const noexcept {
   if (isCommitted()) return true;
   return _empty;
 }
@@ -22,7 +22,7 @@ bool FileVersion::canCommit() const noexcept {
 void FileVersion::commit(fs::path path, mode_t mode) noexcept {
   if (isCommitted()) return;
 
-  ASSERT(canCommit()) << "Attempted to commit unsaved version " << this << " to " << path;
+  ASSERT(isSaved()) << "Attempted to commit unsaved version " << this << " to " << path;
 
   int fd = ::open(path.c_str(), O_WRONLY | O_CREAT | O_TRUNC, mode);
   FAIL_IF(fd < 0) << "Failed to commit empty file version: " << ERR;
