a5b696cd01ebf66be0e36d87b92d02a740d16f45
Charlie Curtsinger <curtsinger@grinnell.edu>
2021-05-03 23:44:17 -0500

    Updated script to run redis builds

--- experiments/case-studies/redis/run.py
+++ experiments/case-studies/redis/run.py
@@ -1,83 +1,38 @@
 #!/usr/bin/env python3
 
-import os
 import sys
+import bench
 
-RKR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'rkr'))
 REPO = 'https://github.com/redis/redis'
 END_COMMIT = 'd96f47cf06b1cc24b82109e0e87ac5428517525a'
 COMMIT_COUNT = 100
 
-def setup(path, commit):
-  print('Setting up working directory at {}'.format(path))
+RKR_COPY_FILES = {'files/Rikerfile': 'Rikerfile'}
 
-  # Make sure the parent directory exists
-  parent_dir = os.path.dirname(path)
-  if parent_dir != '' and not os.path.isdir(parent_dir):
-    print('Cannot clone to {}: Directory {} does not exist'.format(path, parent_dir))
-    exit(1)
+MAKE_SETUP_CMDS = [
+  'make clean'
+]
 
-  # Clone the repository if it doesn't already exist
-  if not os.path.isdir(path):
-    rc = os.system('git clone {} {}'.format(REPO, path))
-    if rc != 0:
-      print('Failed to clone git repository to {}'.format(path))
-      exit(1)
+MAKE_COPY_FILES = {
+  'files/Rikerfile-make': 'Rikerfile'
+}
 
-  else:
-    rc = os.system('cd {}; git reset --hard'.format(path))
-    if rc != 0:
-      print('Failed to reset git repository at {}'.format(path))
-      exit(1)
-  
-  # Now check out the requested commit
-  rc = os.system('cd {}; git checkout {}'.format(path, commit))
-  if rc != 0:
-    print('Failed to check out commit {}'.format(commit))
-    exit(1)
-  
-  # Copy in the Rikerfile
-  rikerfile = os.path.join(os.path.dirname(__file__), 'Rikerfile')
-  rc = os.system('cp {} {}'.format(rikerfile, path))
-  if rc != 0:
-    print('Failed to copy in the Rikerfile')
-    exit(1)
-  
-  # Clean up any remaining riker build state
-  rc = os.system('rm -rf {}'.format(os.path.join(path, '.rkr')))
-  if rc != 0:
-    printf('Failed to clean up remaining riker build state')
-    exit(1)
-  
-  # Run a full build
-  print('Running a full build at commit {}'.format(commit))
-  rc = os.system('cd {}; {} --show'.format(path, RKR))
-  if rc != 0:
-    print('Full build failed')
-    exit(1)
-
-def incremental_build(path, commit):
-  # Check out the requested commit
-  rc = os.system('cd {}; git checkout {}'.format(path, commit))
-  if rc != 0:
-    print('Failed to check out commit {}'.format(commit))
-    exit(1)
-  
-  # Run the incremental build
-  print('Updating build to commit {}'.format(commit))
-  rc = os.system('cd {}; {} --show'.format(path, RKR))
-  if rc != 0:
-    print('Incremental build at commit {} failed'.format(commit))
-    exit(1)
+MAKE_FILTER_CMDS = [
+  'rkr-launch',
+  'Rikerfile',
+  '/bin/sh -c',
+  'make -f'
+]
 
 if __name__ == '__main__':
-  if len(sys.argv) != 2:
+  if len(sys.argv) < 2:
     print('Usage: {} <path to redis checkout>'.format(sys.argv[0]))
     exit(1)
   
-  redis_path = sys.argv[1]
-  setup(redis_path, '{}~{}'.format(END_COMMIT, COMMIT_COUNT))
+  project_path = sys.argv[1]
 
-  for i in reversed(range(0, COMMIT_COUNT)):
-    incremental_build(redis_path, '{}~{}'.format(END_COMMIT, i))
+  # Run the riker build experiment
+  bench.rkr_experiment(project_path, REPO, END_COMMIT, COMMIT_COUNT, RKR_COPY_FILES)
 
+  # Run the make build experiment
+  bench.make_experiment(project_path, REPO, END_COMMIT, COMMIT_COUNT, MAKE_COPY_FILES, MAKE_SETUP_CMDS, MAKE_FILTER_CMDS)
