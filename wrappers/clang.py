9308b4c30adc1eba85b293e730ced1ac17657d66
Michael Chovanak <mchovanak@outlook.com>
2021-06-09 15:53:38 -0500

    fixed clang wrapper -c and -o flags

--- wrappers/clang.py
+++ wrappers/clang.py
@@ -7,25 +7,42 @@ def clang_wrapper(args):
     #     # compile each subsequent arg in a separate process
     #     subprocess.call(["clang", "-c", arg])
     #print(args)
-    if args[2] == "-o":
-        output_name = args[3]
-        subprocess_arr = []
-        o_file_arr = []
-        #print(args[4:])
-        for arg in args[4:]:
-            if ".c" in arg:
-                o_file = arg
-                #o_file[-1] = "o"
-                o_file = o_file[0:-1] + 'o'
-                o_file_arr.append(o_file)
-                subprocess_arr.append(subprocess.Popen(["clang", "-c", "-o", o_file, arg]))
-            elif ".o" in arg:
-                o_file_arr.append(arg)
-        exit_codes = [p.wait() for p in subprocess_arr]
+    subprocess_arr = []
+    c_file_arr = []
+    o_file_arr = []
+    flags = []
+    link = True
+    for i in range(2, len(args)):
+        arg = args[i]
+        if arg == "-o":
+            output_name = args[i+1]
+        elif ".c" in arg:
+            o_file = arg
+            #o_file[-1] = "o"
+            o_file = o_file[0:-1] + 'o'
+            o_file_arr.append(o_file)
+            c_file_arr.append(arg)
+        elif ".o" in arg:
+            o_file_arr.append(arg)
+        elif arg == "-c":
+            link = False
+        elif arg == output_name:
+            continue
+        else:
+            print("Unrecognized flag: " + arg + "\n")
+            subprocess.call(args[1:])
+            quit
+    for arg in c_file_arr:
+        o_file = arg
+        o_file = o_file[0:-1] + 'o'
+        compile_args = ["clang", "-c", "-o", o_file, arg] + flags
+        subprocess_arr.append(subprocess.Popen(compile_args))
+    exit_codes = [p.wait() for p in subprocess_arr]
+    if link:
         link_args = ["clang", "-o", output_name] + o_file_arr
-        # link_args.append(o_file_arr)
-        #print(link_args)
         subprocess.call(link_args)
+    
+        
     # TODO: account for the case of compiling w/o linking, whether a single .c file or multiple
     # elif arg[2] == "-c": 
     #     compile_args = ["clang", "-c", "-o", ]
