2321cb0873718e8107170ebc5c09be16623f8a1e
Daniel W. Barowy <dbarowy@cs.williams.edu>
2021-12-21 18:20:48 -0500

    Cool.  It works.

--- utils/rewrite_shell/lib/pretty_printer.py
+++ utils/rewrite_shell/lib/pretty_printer.py
@@ -1,195 +1,104 @@
 from typing import TYPE_CHECKING
 from bashlex import ast
 
-class prettyprinter(ast.nodevisitor):
 
+class prettyprinter(ast.nodevisitor):
     def __init__(self):
         self.indent = 0
 
     def visitoperator(self, n, op):
-        print(" " * self.indent + str(n))
+        return op + " "
 
     def visitlist(self, n, parts):
-        print(" " * self.indent + str(n))
+        return "( " + " ".join(map(lambda part: self.visit(part), parts)) + " ) "
 
     def visitpipe(self, n, pipe):
-        print(" " * self.indent + str(n))
+        return pipe + " "
 
     def visitpipeline(self, n, parts):
-        print(" " * self.indent + str(n))
+        return "[TODO pipeline] "
 
     def visitcompound(self, n, list, redirects):
-        print(" " * self.indent + str(n))
+        return "[TODO compound] "
 
     def visitif(self, n, parts):
-        print(" " * self.indent + str(n))
+        return "[TODO if] "
 
     def visitfor(self, n, parts):
-        print(" " * self.indent + str(n))
+        return "[TODO for] "
 
     def visitwhile(self, n, parts):
-        print(" " * self.indent + str(n))
+        return "[TODO while] "
 
     def visituntil(self, n, parts):
-        print(" " * self.indent + str(n))
+        return "[TODO until] "
 
     def visitcommand(self, n, parts):
-        print(" " * self.indent + "CommandNode")
+        return " ".join(map(lambda part: self.visit(part), parts)) + " "
 
     def visitfunction(self, n, name, body, parts):
-        print(" " * self.indent + str(n))
+        return "[TODO function] "
 
     def visitword(self, n, word):
-        if len(n.parts) == 0:
-            print(" " * self.indent + "WordNode(word = '" + word + "')")
-        else:
-            print(" " * self.indent + "WordNode")
+        return word + " "
 
     def visitassignment(self, n, word):
-        [lhs, rhs] = word.split("=")
-        print(" " * self.indent + "AssignmentNode(lhs = '" + lhs + "', rhs = '" + rhs + "')")
+        return word + " "
 
     def visitreservedword(self, n, word):
-        print(" " * self.indent + str(n))
+        return word + " "
 
     def visitparameter(self, n, value):
-        print(" " * self.indent + "ParameterNode(value = '" + value + "')")
+        return value + " "
 
     def visittilde(self, n, value):
-        print(" " * self.indent + str(n))
+        return value + " "
 
     def visitredirect(self, n, input, type, output, heredoc):
+        if heredoc != None:
+            return self.visit(heredoc)
         if input != None and output == None:
-            print(" " * self.indent + "RedirectInputNode")
+            return type + " " + self.visit(input) + " "
         elif input == None and output != None:
-            print(" " * self.indent + "RedirectOutputNode")
-        elif input != None and output != None:
-            print(" " * self.indent + "RedirectInputAndOutputNode")
+            return type + " " + self.visit(output) + " "
         else:
-            print(" " * self.indent + "RedirectNothing???")
+            return "[TODO some other kind of redirect] "
 
     def visitheredoc(self, n, value):
-        print(" " * self.indent + str(n))
+        return value + " "
 
     def visitprocesssubstitution(self, n, command):
-        print(" " * self.indent + str(n))
+        return self.visit(command) + " "
 
     def visitcommandsubstitution(self, n, command):
-        print(" " * self.indent + str(n))
+        return self.visit(command) + " "
 
+    # This redefinition exists to push all the work
+    # inside each visitor.
     def visit(self, n):
         k = n.kind
         if k == "operator":
-            self.indent += 2
-            self._visitnode(n, n.op)
-            self.indent -= 2
-        elif k == "list":
-            self.indent += 2
-            dochild = self._visitnode(n, n.parts)
-            if dochild is None or dochild:
-                self.indent += 2
-                for child in n.parts:
-                    self.visit(child)
-                self.indent -= 2
-            self.indent -= 2
-        elif k == "reservedword":
-            self.indent += 2
-            self._visitnode(n, n.word)
-            self.indent -= 2
+            return self._visitnode(n, n.op)
         elif k == "pipe":
-            self.indent += 2
-            self._visitnode(n, n.pipe)
-            self.indent -= 2
-        elif k == "pipeline":
-            self.indent += 2
-            dochild = self._visitnode(n, n.parts)
-            self.indent -= 2
-            if dochild is None or dochild:
-                self.indent += 2
-                for child in n.parts:
-                    self.visit(child)
-                self.indent -= 2
+            return self._visitnode(n, n.pipe)
         elif k == "compound":
-            self.indent += 2
-            dochild = self._visitnode(n, n.list, n.redirects)
-            if dochild is None or dochild:
-                self.indent += 2
-                for child in n.list:
-                    self.visit(child)
-                for child in n.redirects:
-                    self.visit(child)
-                self.indent -= 2
-            self.indent -= 2
-        elif k in ("if", "for", "while", "until"):
-            self.indent += 2
-            dochild = self._visitnode(n, n.parts)
-            if dochild is None or dochild:
-                self.indent += 2
-                for child in n.parts:
-                    self.visit(child)
-                self.indent -= 2
-            self.indent -= 2
-        elif k == "command":
-            self.indent += 2
-            dochild = self._visitnode(n, n.parts)
-            if dochild is None or dochild:
-                self.indent += 2
-                for child in n.parts:
-                    self.visit(child)
-                self.indent -= 2
-            self.indent -= 2
+            return self._visitnode(n, n.list, n.redirects)
+        elif k in ("if", "for", "while", "until", "command", "pipeline", "list"):
+            return self._visitnode(n, n.parts)
         elif k == "function":
-            self.indent += 2
-            dochild = self._visitnode(n, n.name, n.body, n.parts)
-            if dochild is None or dochild:
-                self.indent += 2
-                for child in n.parts:
-                    self.visit(child)
-                self.indent -= 2
-            self.indent -= 2
+            return self._visitnode(n, n.name, n.body, n.parts)
         elif k == "redirect":
-            self.indent += 2
-            dochild = self._visitnode(n, n.input, n.type, n.output, n.heredoc)
-            if dochild is None or dochild:
-                self.indent += 2
-                if isinstance(n.output, ast.node):
-                    self.visit(n.output)
-                if n.heredoc:
-                    self.visit(n.heredoc)
-                self.indent -= 2
-            self.indent -= 2
-        elif k == "word":
-            self.indent += 2
-            dochild = self._visitnode(n, n.word)
-            if dochild is None or dochild:
-                self.indent += 2
-                for child in n.parts:
-                    self.visit(child)
-                self.indent -= 2
-            self.indent -= 2
-        elif k == "assignment":
-            self.indent += 2
-            dochild = self._visitnode(n, n.word)
-            if dochild is None or dochild:
-                self.indent += 2
-                for child in n.parts:
-                    self.visit(child)
-                self.indent -= 2
-            self.indent -= 2
+            return self._visitnode(n, n.input, n.type, n.output, n.heredoc)
+        elif k in ("word", "assignment", "reservedword"):
+            return self._visitnode(n, n.word)
         elif k in ("parameter", "tilde", "heredoc"):
-            self.indent += 2
-            self._visitnode(n, n.value)
-            self.indent -= 2
+            return self._visitnode(n, n.value)
         elif k in ("commandsubstitution", "processsubstitution"):
-            self.indent += 2
-            dochild = self._visitnode(n, n.command)
-            if dochild is None or dochild:
-                self.indent += 2
-                self.visit(n.command)
-                self.indent -= 2
-            self.indent -= 2
+            return self._visitnode(n, n.command)
         else:
             raise ValueError("unknown node kind %r" % k)
-        self.indent += 2
-        self.visitnodeend(n)
-        self.indent -= 2
+
+    @staticmethod
+    def print(tree):
+        pp = prettyprinter()
+        print(pp.visit(tree))
