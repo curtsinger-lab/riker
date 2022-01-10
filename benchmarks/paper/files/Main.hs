12bfa79965e625d497522eaa0c57515fa639bc9c
Daniel W. Barowy <dbarowy@cs.williams.edu>
2022-01-10 17:24:15 -0500

    add 'Shell' to commands

--- benchmarks/paper/files/Main.hs
+++ benchmarks/paper/files/Main.hs
@@ -4,12 +4,12 @@ import Development.Rattle
 
 main :: IO ()
 main = rattleRun rattleOptions $ do
-  cmd "pdflatex  main.tex"
-  cmd "bibtex  main.aux"
-  cmd "pdflatex  main.tex"
-  cmd "pdflatex  main.tex"
-  cmd "pdflatex  extended-abstract.tex"
-  cmd "bibtex  extended-abstract.aux"
-  cmd "pdflatex  extended-abstract.tex"
-  cmd "pdflatex  extended-abstract.tex"
+  cmd Shell "pdflatex  main.tex"
+  cmd Shell "bibtex  main.aux"
+  cmd Shell "pdflatex  main.tex"
+  cmd Shell "pdflatex  main.tex"
+  cmd Shell "pdflatex  extended-abstract.tex"
+  cmd Shell "bibtex  extended-abstract.aux"
+  cmd Shell "pdflatex  extended-abstract.tex"
+  cmd Shell "pdflatex  extended-abstract.tex"
 
