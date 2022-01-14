4475613c4c4b6cc5c341565f39549aaa627736b4
Charlie Curtsinger <curtsinger@grinnell.edu>
2022-01-14 11:18:17 -0600

    Whoops, the arm64 syscalls ended up under amd64

--- src/rkr/tracing/amd64/syscalls.hh
+++ src/rkr/tracing/amd64/syscalls.hh
@@ -4,307 +4,341 @@
 #error "syscalls.hh was included without the TRACE macro"
 #endif
 
-/* 000 */ // skip io_setup
-/* 001 */ // skip io_destroy
-/* 002 */ // skip io_submit
-/* 003 */ // skip io_cancel
-/* 004 */ // skip io_getevents
-/* 005 */ // skip setxattr
-/* 006 */ // skip lsetxattr
-/* 007 */ // skip fsetxattr
-/* 008 */ // skip getxattr
-/* 009 */ // skip lgetxattr
-/* 010 */ // skip fgetxattr
-/* 011 */ // skip listxattr
-/* 012 */ // skip llistxattr
-/* 013 */ // skip flistxattr
-/* 014 */ // skip removexattr
-/* 015 */ // skip lremovexattr
-/* 016 */ // skip fremovexattr
-/* 017 */ // skip getcwd
-/* 018 */ // skip lookup_dcookie
-/* 019 */ // skip eventfd2
-/* 020 */ // skip epoll_create1
-/* 021 */ // skip epoll_ctl
-/* 022 */ // skip epoll_pwait
-/* 023 */ TRACE(dup);
-/* 024 */ TRACE(dup3);
-/* 025 */ TRACE(fcntl);
-/* 026 */ // skip inotify_init1
-/* 027 */ // skip inotify_add_watch
-/* 028 */ // skip inotify_rm_watch
-/* 029 */ // skip ioctl
-/* 030 */ // skip ioprio_set
-/* 031 */ // skip ioprio_get
-/* 032 */ // skip flock
-/* 033 */ TRACE(mknodat);
-/* 034 */ TRACE(mkdirat);
-/* 035 */ TRACE(unlinkat);
-/* 036 */ TRACE(symlinkat);
-/* 037 */ TRACE(linkat);
-/* 038 */ TRACE(renameat);
-/* 039 */ // skip umount2
-/* 040 */ // skip mount
-/* 041 */ TRACE(pivot_root);
-/* 042 */ // skip nfsservctl
-/* 043 */ // skip statfs
-/* 044 */ // skip fstatfs
-/* 045 */ TRACE(truncate);
-/* 046 */ TRACE(ftruncate);
-/* 047 */ // skip fallocate
-/* 048 */ TRACE(faccessat);
-/* 049 */ TRACE(chdir);
-/* 050 */ TRACE(fchdir);
-/* 051 */ TRACE(chroot);
-/* 052 */ TRACE(fchmod);
-/* 053 */ TRACE(fchmodat);
-/* 054 */ TRACE(fchownat);
-/* 055 */ TRACE(fchown);
-/* 056 */ TRACE(openat);
-/* 057 */ TRACE(close);
-/* 058 */ // skip vhangup
-/* 059 */ TRACE(pipe2);
-/* 060 */ // skip quotactl
-/* 061 */ TRACE(getdents64);
-/* 062 */ // skip lseek
-/* 063 */ TRACE(read);
-/* 064 */ TRACE(write);
-/* 065 */ TRACE(readv);
-/* 066 */ TRACE(writev);
-/* 067 */ TRACE(pread64);
-/* 068 */ TRACE(pwrite64);
-/* 069 */ TRACE(preadv);
-/* 070 */ TRACE(pwritev);
-/* 071 */ TRACE(sendfile);
-/* 072 */ // skip pselect6
-/* 073 */ // skip ppoll
-/* 074 */ // skip signalfd4
-/* 075 */ TRACE(vmsplice);
-/* 076 */ TRACE(splice);
-/* 077 */ TRACE(tee);
-/* 078 */ TRACE(readlinkat);
-/* 079 */ TRACE(fstatat);
-/* 080 */ TRACE(fstat);
-/* 081 */ // skip sync
-/* 082 */ // skip fsync
-/* 083 */ // skip fdatasync
-/* 084 */ // skip sync_file_range2
-/* 084 */ // skip sync_file_range
-/* 085 */ // skip timerfd_create
-/* 086 */ // skip timerfd_settime
-/* 087 */ // skip timerfd_gettime
-/* 088 */ // skip utimensat
-/* 089 */ // skip acct
-/* 090 */ // skip capget
-/* 091 */ // skip capset
-/* 092 */ // skip personality
-/* 093 */ // skip exit
-/* 094 */ // skip exit_group
-/* 095 */ TRACE(waitid);
-/* 096 */ // skip set_tid_address
-/* 097 */ // skip unshare
-/* 098 */ // skip futex
-/* 099 */ // skip set_robust_list
-/* 100 */ // skip get_robust_list
-/* 101 */ // skip nanosleep
-/* 102 */ // skip getitimer
-/* 103 */ // skip setitimer
-/* 104 */ // skip kexec_load
-/* 105 */ // skip init_module
-/* 106 */ // skip delete_module
-/* 107 */ // skip timer_create
-/* 108 */ // skip timer_gettime
-/* 109 */ // skip timer_getoverrun
-/* 110 */ // skip timer_settime
-/* 111 */ // skip timer_delete
-/* 112 */ // skip clock_settime
-/* 113 */ // skip clock_gettime
-/* 114 */ // skip clock_getres
-/* 115 */ // skip clock_nanosleep
-/* 116 */ // skip syslog
-/* 117 */ // skip ptrace
-/* 118 */ // skip sched_setparam
-/* 119 */ // skip sched_setscheduler
-/* 120 */ // skip sched_getscheduler
-/* 121 */ // skip sched_getparam
-/* 122 */ // skip sched_setaffinity
-/* 123 */ // skip sched_getaffinity
-/* 124 */ // skip sched_yield
-/* 125 */ // skip sched_get_priority_max
-/* 126 */ // skip sched_get_priority_min
-/* 127 */ // skip sched_rr_get_interval
-/* 128 */ // skip restart_syscall
-/* 129 */ // skip kill
-/* 130 */ // skip tkill
-/* 131 */ // skip tgkill
-/* 132 */ // skip sigaltstack
-/* 133 */ // skip rt_sigsuspend
-/* 134 */ // skip rt_sigaction
-/* 135 */ // skip rt_sigprocmask
-/* 136 */ // skip rt_sigpending
-/* 137 */ // skip rt_sigtimedwait
-/* 138 */ // skip rt_sigqueueinfo
-/* 139 */ // skip rt_sigreturn
-/* 140 */ // skip setpriority
-/* 141 */ // skip getpriority
-/* 142 */ // skip reboot
-/* 143 */ // skip setregid
-/* 144 */ // skip setgid
-/* 145 */ // skip setreuid
-/* 146 */ // skip setuid
-/* 147 */ // skip setresuid
-/* 148 */ // skip getresuid
-/* 149 */ // skip setresgid
-/* 150 */ // skip getresgid
-/* 151 */ // skip setfsuid
-/* 152 */ // skip setfsgid
-/* 153 */ // skip times
-/* 154 */ // skip setpgid
-/* 155 */ // skip getpgid
-/* 156 */ // skip getsid
-/* 157 */ // skip setsid
-/* 158 */ // skip getgroups
-/* 159 */ // skip setgroups
-/* 160 */ // skip uname
-/* 161 */ // skip sethostname
-/* 162 */ // skip setdomainname
-/* 163 */ // skip getrlimit
-/* 164 */ // skip setrlimit
-/* 165 */ // skip getrusage
-/* 166 */ TRACE(umask);
-/* 167 */ // skip prctl
-/* 168 */ // skip getcpu
-/* 169 */ // skip gettimeofday
-/* 170 */ // skip settimeofday
-/* 171 */ // skip adjtimex
-/* 172 */ // skip getpid
-/* 173 */ // skip getppid
-/* 174 */ // skip getuid
-/* 175 */ // skip geteuid
-/* 176 */ // skip getgid
-/* 177 */ // skip getegid
-/* 178 */ // skip gettid
-/* 179 */ // skip sysinfo
-/* 180 */ // skip mq_open
-/* 181 */ // skip mq_unlink
-/* 182 */ // skip mq_timedsend
-/* 183 */ // skip mq_timedreceive
-/* 184 */ // skip mq_notify
-/* 185 */ // skip mq_getsetattr
-/* 186 */ // skip msgget
-/* 187 */ // skip msgctl
-/* 188 */ // skip msgrcv
-/* 189 */ // skip msgsnd
-/* 190 */ // skip semget
-/* 191 */ // skip semctl
-/* 192 */ // skip semtimedop
-/* 193 */ // skip semop
-/* 194 */ // skip shmget
-/* 195 */ // skip shmctl
-/* 196 */ // skip shmat
-/* 197 */ // skip shmdt
-/* 198 */ TRACE(socket);
-/* 199 */ TRACE(socketpair);
-/* 200 */ // skip bind
-/* 201 */ // skip listen
-/* 202 */ // skip accept
-/* 203 */ // skip connect
-/* 204 */ // skip getsockname
-/* 205 */ // skip getpeername
-/* 206 */ TRACE(sendto);
-/* 207 */ TRACE(recvfrom);
-/* 208 */ // skip setsockopt
-/* 209 */ // skip getsockopt
-/* 210 */ // skip shutdown
-/* 211 */ TRACE(sendmsg);
-/* 212 */ TRACE(recvmsg);
-/* 213 */ // skip readahead
-/* 214 */ // skip brk
-/* 215 */ // skip munmap
-/* 216 */ // skip mremap
-/* 217 */ // skip add_key
-/* 218 */ // skip request_key
-/* 219 */ // skip keyctl
-/* 220 */ // skip clone
-/* 221 */ TRACE(execve);
-/* 222 */ TRACE(mmap);
-/* 223 */ // skip fadvise64
-/* 224 */ // skip swapon
-/* 225 */ // skip swapoff
-/* 226 */ // skip mprotect
-/* 227 */ // skip msync
-/* 228 */ // skip mlock
-/* 229 */ // skip munlock
-/* 230 */ // skip mlockall
-/* 231 */ // skip munlockall
-/* 232 */ // skip mincore
-/* 233 */ // skip madvise
-/* 234 */ // skip remap_file_pages
-/* 235 */ // skip mbind
-/* 236 */ // skip get_mempolicy
-/* 237 */ // skip set_mempolicy
-/* 238 */ // skip migrate_pages
-/* 239 */ // skip move_pages
-/* 240 */ // skip rt_tgsigqueueinfo
-/* 241 */ // skip perf_event_open
-/* 242 */ // skip accept4
-/* 243 */ // skip recvmmsg
-/* 244 */ // skip arch_specific_syscall
-/* 260 */ TRACE(wait4);
-/* 261 */ // skip prlimit64
-/* 262 */ // skip fanotify_init
-/* 263 */ // skip fanotify_mark
-/* 264 */ // skip name_to_handle_at
-/* 265 */ // skip open_by_handle_at
-/* 266 */ // skip clock_adjtime
-/* 267 */ // skip syncfs
-/* 268 */ // skip setns
-/* 269 */ // skip sendmmsg
-/* 270 */ // skip process_vm_readv
-/* 271 */ // skip process_vm_writev
-/* 272 */ // skip kcmp
-/* 273 */ // skip finit_module
-/* 274 */ // skip sched_setattr
-/* 275 */ // skip sched_getattr
-/* 276 */ TRACE(renameat2);
-/* 277 */ // skip seccomp
-/* 278 */ // skip getrandom
-/* 279 */ // skip memfd_create
-/* 280 */ // skip bpf
-/* 281 */ TRACE(execveat);
-/* 282 */ // skip userfaultfd
-/* 283 */ // skip membarrier
-/* 284 */ // skip mlock2
-/* 285 */ TRACE(copy_file_range);
-/* 286 */ TRACE(preadv2);
-/* 287 */ TRACE(pwritev2);
-/* 288 */ // skip pkey_mprotect
-/* 289 */ // skip pkey_alloc
-/* 290 */ // skip pkey_free
-/* 291 */ TRACE(statx);
-/* 292 */ // skip io_pgetevents
-/* 293 */ // skip rseq
-/* 294 */ // skip kexec_file_load
-/* 403 */ // skip clock_gettime64
-/* 404 */ // skip clock_settime64
-/* 405 */ // skip clock_adjtime64
-/* 406 */ // skip clock_getres_time64
-/* 407 */ // skip clock_nanosleep_time64
-/* 408 */ // skip timer_gettime64
-/* 409 */ // skip timer_settime64
-/* 410 */ // skip timerfd_gettime64
-/* 411 */ // skip timerfd_settime64
-/* 412 */ // skip utimensat_time64
-/* 413 */ // skip pselect6_time64
-/* 414 */ // skip ppoll_time64
-/* 416 */ // skip io_pgetevents_time64
-/* 417 */ // skip recvmmsg_time64
-/* 418 */ // skip mq_timedsend_time64
-/* 419 */ // skip mq_timedreceive_time64
-/* 420 */ // skip semtimedop_time64
-/* 421 */ // skip rt_sigtimedwait_time64
-/* 422 */ // skip futex_time64
-/* 423 */ // skip sched_rr_get_interval_time64
+/* 000 */ TRACE(read);
+/* 001 */ TRACE(write);
+/* 002 */ TRACE(open);
+/* 003 */ TRACE(close);
+/* 004 */ TRACE(stat);
+/* 005 */ TRACE(fstat);
+/* 006 */ TRACE(lstat);
+/* 007 */ // skip poll
+/* 008 */ // skip lseek
+/* 009 */ TRACE(mmap);
+/* 010 */ // skip mprotect
+/* 011 */ // skip munmap
+/* 012 */ // skip brk
+/* 013 */ // skip rt_sigaction
+/* 014 */ // skip rt_sigprocmask
+/* 015 */ // skip rt_sigreturn
+/* 016 */ // skip ioctl
+/* 017 */ TRACE(pread64);
+/* 018 */ TRACE(pwrite64);
+/* 019 */ TRACE(readv);
+/* 020 */ TRACE(writev);
+/* 021 */ TRACE(access);
+/* 022 */ TRACE(pipe);
+/* 023 */ // skip select
+/* 024 */ // skip sched_yield
+/* 025 */ // skip mremap
+/* 026 */ // skip msync
+/* 027 */ // skip mincore
+/* 028 */ // skip madvise
+/* 029 */ // skip shmget
+/* 030 */ // skip shmat
+/* 031 */ // skip shmctl
+/* 032 */ TRACE(dup);
+/* 033 */ TRACE(dup2);
+/* 034 */ // skip pause
+/* 035 */ // skip nanosleep
+/* 036 */ // skip getitimer
+/* 037 */ // skip alarm
+/* 038 */ // skip setitimer
+/* 039 */ // skip getpid
+/* 040 */ TRACE(sendfile);
+/* 041 */ TRACE(socket);
+/* 042 */ // skip connect
+/* 043 */ // skip accept
+/* 044 */ TRACE(sendto);
+/* 045 */ TRACE(recvfrom);
+/* 046 */ TRACE(sendmsg);
+/* 047 */ TRACE(recvmsg);
+/* 048 */ // skip shutdown
+/* 049 */ // skip bind
+/* 050 */ // skip listen
+/* 051 */ // skip getsockname
+/* 052 */ // skip getpeername
+/* 053 */ TRACE(socketpair);
+/* 054 */ // skip setsockopt
+/* 055 */ // skip getsockopt
+/* 056 */ // skip clone
+/* 057 */ // skip fork
+/* 058 */ // skip vfork
+/* 059 */ TRACE(execve);
+/* 060 */ // skip exit
+/* 061 */ TRACE(wait4);
+/* 062 */ // skip kill
+/* 063 */ // skip uname
+/* 064 */ // skip semget
+/* 065 */ // skip semop
+/* 066 */ // skip semctl
+/* 067 */ // skip shmdt
+/* 068 */ // skip msgget
+/* 069 */ // skip msgsnd
+/* 070 */ // skip msgrcv
+/* 071 */ // skip msgctl
+/* 072 */ TRACE(fcntl);
+/* 073 */ // skip flock
+/* 074 */ // skip fsync
+/* 075 */ // skip fdatasync
+/* 076 */ TRACE(truncate);
+/* 077 */ TRACE(ftruncate);
+/* 078 */ TRACE(getdents);
+/* 079 */ // skip getcwd
+/* 080 */ TRACE(chdir);
+/* 081 */ TRACE(fchdir);
+/* 082 */ TRACE(rename);
+/* 083 */ TRACE(mkdir);
+/* 084 */ TRACE(rmdir);
+/* 085 */ TRACE(creat);
+/* 086 */ TRACE(link);
+/* 087 */ TRACE(unlink);
+/* 088 */ TRACE(symlink);
+/* 089 */ TRACE(readlink);
+/* 090 */ TRACE(chmod);
+/* 091 */ TRACE(fchmod);
+/* 092 */ TRACE(chown);
+/* 093 */ TRACE(fchown);
+/* 094 */ TRACE(lchown);
+/* 095 */ TRACE(umask);
+/* 096 */ // skip gettimeofday
+/* 097 */ // skip getrlimit
+/* 098 */ // skip getrusage
+/* 099 */ // skip sysinfo
+/* 100 */ // skip times
+/* 101 */ // skip ptrace
+/* 102 */ // skip getuid
+/* 103 */ // skip syslog
+/* 104 */ // skip getgid
+/* 105 */ // skip setuid
+/* 106 */ // skip setgid
+/* 107 */ // skip geteuid
+/* 108 */ // skip getegid
+/* 109 */ // skip setpgid
+/* 110 */ // skip getppid
+/* 111 */ // skip getpgrp
+/* 112 */ // skip setsid
+/* 113 */ // skip setreuid
+/* 114 */ // skip setregid
+/* 115 */ // skip getgroups
+/* 116 */ // skip setgroups
+/* 117 */ // skip setresuid
+/* 118 */ // skip getresuid
+/* 119 */ // skip setresgid
+/* 120 */ // skip getresgid
+/* 121 */ // skip getpgid
+/* 122 */ // skip setfsuid
+/* 123 */ // skip setfsgid
+/* 124 */ // skip getsid
+/* 125 */ // skip capget
+/* 126 */ // skip capset
+/* 127 */ // skip rt_sigpending
+/* 128 */ // skip rt_sigtimedwait
+/* 129 */ // skip rt_sigqueueinfo
+/* 130 */ // skip rt_sigsuspend
+/* 131 */ // skip sigaltstack
+/* 132 */ // skip utime
+/* 133 */ TRACE(mknod);
+/* 134 */ // skip uselib
+/* 135 */ // skip personality
+/* 136 */ // skip ustat
+/* 137 */ // skip statfs
+/* 138 */ // skip fstatfs
+/* 139 */ // skip sysfs
+/* 140 */ // skip getpriority
+/* 141 */ // skip setpriority
+/* 142 */ // skip sched_setparam
+/* 143 */ // skip sched_getparam
+/* 144 */ // skip sched_setscheduler
+/* 145 */ // skip sched_getscheduler
+/* 146 */ // skip sched_get_priority_max
+/* 147 */ // skip sched_get_priority_min
+/* 148 */ // skip sched_rr_get_interval
+/* 149 */ // skip mlock
+/* 150 */ // skip munlock
+/* 151 */ // skip mlockall
+/* 152 */ // skip munlockall
+/* 153 */ // skip vhangup
+/* 154 */ // skip modify_ldt
+/* 155 */ TRACE(pivot_root);
+/* 156 */ // skip _sysctl
+/* 157 */ // skip prctl
+/* 158 */ // skip arch_prctl
+/* 159 */ // skip adjtimex
+/* 160 */ // skip setrlimit
+/* 161 */ TRACE(chroot);
+/* 162 */ // skip sync
+/* 163 */ // skip acct
+/* 164 */ // skip settimeofday
+/* 165 */ // skip mount
+/* 166 */ // skip umount2
+/* 167 */ // skip swapon
+/* 168 */ // skip swapoff
+/* 169 */ // skip reboot
+/* 170 */ // skip sethostname
+/* 171 */ // skip setdomainname
+/* 172 */ // skip iopl
+/* 173 */ // skip ioperm
+/* 174 */ // skip create_module
+/* 175 */ // skip init_module
+/* 176 */ // skip delete_module
+/* 177 */ // skip get_kernel_syms
+/* 178 */ // skip query_module
+/* 179 */ // skip quotactl
+/* 180 */ // skip nfsservctl
+/* 181 */ // skip getpmsg
+/* 182 */ // skip putpmsg
+/* 183 */ // skip afs_syscall
+/* 184 */ // skip tuxcall
+/* 185 */ // skip security
+/* 186 */ // skip gettid
+/* 187 */ // skip readahead
+/* 188 */ // skip setxattr
+/* 189 */ // skip lsetxattr
+/* 190 */ // skip fsetxattr
+/* 191 */ // skip getxattr
+/* 192 */ // skip lgetxattr
+/* 193 */ // skip fgetxattr
+/* 194 */ // skip listxattr
+/* 195 */ // skip llistxattr
+/* 196 */ // skip flistxattr
+/* 197 */ // skip removexattr
+/* 198 */ // skip lremovexattr
+/* 199 */ // skip fremovexattr
+/* 200 */ // skip tkill
+/* 201 */ // skip time
+/* 202 */ // skip futex
+/* 203 */ // skip sched_setaffinity
+/* 204 */ // skip sched_getaffinity
+/* 205 */ // skip set_thread_area
+/* 206 */ // skip io_setup
+/* 207 */ // skip io_destroy
+/* 208 */ // skip io_getevents
+/* 209 */ // skip io_submit
+/* 210 */ // skip io_cancel
+/* 211 */ // skip get_thread_area
+/* 212 */ // skip lookup_dcookie
+/* 213 */ // skip epoll_create
+/* 214 */ // skip epoll_ctl_old
+/* 215 */ // skip epoll_wait_old
+/* 216 */ // skip remap_file_pages
+/* 217 */ TRACE(getdents64);
+/* 218 */ // skip set_tid_address
+/* 219 */ // skip restart_syscall
+/* 220 */ // skip semtimedop
+/* 221 */ // skip fadvise64
+/* 222 */ // skip timer_create
+/* 223 */ // skip timer_settime
+/* 224 */ // skip timer_gettime
+/* 225 */ // skip timer_getoverrun
+/* 226 */ // skip timer_delete
+/* 227 */ // skip clock_settime
+/* 228 */ // skip clock_gettime
+/* 229 */ // skip clock_getres
+/* 230 */ // skip clock_nanosleep
+/* 231 */ // skip exit_group
+/* 232 */ // skip epoll_wait
+/* 233 */ // skip epoll_ctl
+/* 234 */ // skip tgkill
+/* 235 */ // skip utimes
+/* 236 */ // skip vserver
+/* 237 */ // skip mbind
+/* 238 */ // skip set_mempolicy
+/* 239 */ // skip get_mempolicy
+/* 240 */ // skip mq_open
+/* 241 */ // skip mq_unlink
+/* 242 */ // skip mq_timedsend
+/* 243 */ // skip mq_timedreceive
+/* 244 */ // skip mq_notify
+/* 245 */ // skip mq_getsetattr
+/* 246 */ // skip kexec_load
+/* 247 */ TRACE(waitid);
+/* 248 */ // skip add_key
+/* 249 */ // skip request_key
+/* 250 */ // skip keyctl
+/* 251 */ // skip ioprio_set
+/* 252 */ // skip ioprio_get
+/* 253 */ // skip inotify_init
+/* 254 */ // skip inotify_add_watch
+/* 255 */ // skip inotify_rm_watch
+/* 256 */ // skip migrate_pages
+/* 257 */ TRACE(openat);
+/* 258 */ TRACE(mkdirat);
+/* 259 */ TRACE(mknodat);
+/* 260 */ TRACE(fchownat);
+/* 261 */ // skip futimesat
+/* 262 */ TRACE(newfstatat);
+/* 263 */ TRACE(unlinkat);
+/* 264 */ TRACE(renameat);
+/* 265 */ TRACE(linkat);
+/* 266 */ TRACE(symlinkat);
+/* 267 */ TRACE(readlinkat);
+/* 268 */ TRACE(fchmodat);
+/* 269 */ TRACE(faccessat);
+/* 270 */ // skip pselect6
+/* 271 */ // skip ppoll
+/* 272 */ // skip unshare
+/* 273 */ // skip set_robust_list
+/* 274 */ // skip get_robust_list
+/* 275 */ TRACE(splice);
+/* 276 */ TRACE(tee);
+/* 277 */ // skip sync_file_range
+/* 278 */ TRACE(vmsplice);
+/* 279 */ // skip move_pages
+/* 280 */ // skip utimensat
+/* 281 */ // skip epoll_pwait
+/* 282 */ // skip signalfd
+/* 283 */ // skip timerfd_create
+/* 284 */ // skip eventfd
+/* 285 */ // skip fallocate
+/* 286 */ // skip timerfd_settime
+/* 287 */ // skip timerfd_gettime
+/* 288 */ // skip accept4
+/* 289 */ // skip signalfd4
+/* 290 */ // skip eventfd2
+/* 291 */ // skip epoll_create1
+/* 292 */ TRACE(dup3);
+/* 293 */ TRACE(pipe2);
+/* 294 */ // skip inotify_init1
+/* 295 */ TRACE(preadv);
+/* 296 */ TRACE(pwritev);
+/* 297 */ // skip rt_tgsigqueueinfo
+/* 298 */ // skip perf_event_open
+/* 299 */ // skip recvmmsg
+/* 300 */ // skip fanotify_init
+/* 301 */ // skip fanotify_mark
+/* 302 */ // skip prlimit64
+/* 303 */ // skip name_to_handle_at
+/* 304 */ // skip open_by_handle_at
+/* 305 */ // skip clock_adjtime
+/* 306 */ // skip syncfs
+/* 307 */ // skip sendmmsg
+/* 308 */ // skip setns
+/* 309 */ // skip getcpu
+/* 310 */ // skip process_vm_readv
+/* 311 */ // skip process_vm_writev
+/* 312 */ // skip kcmp
+/* 313 */ // skip finit_module
+/* 314 */ // skip sched_setattr
+/* 315 */ // skip sched_getattr
+/* 316 */ TRACE(renameat2);
+/* 317 */ // skip seccomp
+/* 318 */ // skip getrandom
+/* 319 */ // skip memfd_create
+/* 320 */ // skip kexec_file_load
+/* 321 */ // skip bpf
+/* 322 */ TRACE(execveat);
+/* 323 */ // skip userfaultfd
+/* 324 */ // skip membarrier
+/* 325 */ // skip mlock2
+/* 326 */ TRACE(copy_file_range);
+/* 327 */ TRACE(preadv2);
+/* 328 */ TRACE(pwritev2);
+/* 329 */ // skip pkey_mprotect
+/* 330 */ // skip pkey_alloc
+/* 331 */ // skip pkey_free
+/* 332 */ TRACE(statx);
+/* 333 */ // skip io_pgetevents
+/* 334 */ // skip rseq
 /* 424 */ // skip pidfd_send_signal
 /* 425 */ // skip io_uring_setup
 /* 426 */ // skip io_uring_enter
