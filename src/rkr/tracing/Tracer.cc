25e462b058eb68336c62244d5bf8a4b8f89440fd
Eric Zhu <eric@ericz.me>
2023-03-25 19:55:27 -0400

    feat(tracer): eperm it

--- src/rkr/tracing/Tracer.cc
+++ src/rkr/tracing/Tracer.cc
@@ -556,7 +556,7 @@ shared_ptr<Process> Tracer::launchTraced(Build& build, const shared_ptr<Command>
       } else if (SyscallTable<Build>::get(i).isBlocked()) {
           bpf.push_back(BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, i, 0, 1));
           //bpf.push_back(BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW));
-          bpf.push_back(BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ERRNO | (EADV & SECCOMP_RET_DATA)));
+          bpf.push_back(BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ERRNO | (EPERM & SECCOMP_RET_DATA)));
           //bpf.push_back(BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_TRACE));
       } else {
         if (SyscallTable<Build>::get(i).isTraced()) {
