eb64d2544f9d770c6bf4933d4cd89cc06e5c238d
Zimmerea <zimmerma4@grinnell.edu>
2023-06-20 10:26:17 -0500

    Make ssh-wrapper actually run ssh. Runs from ssh in bin

--- src/ssh-wrapper/ssh-wrapper.cc
+++ src/ssh-wrapper/ssh-wrapper.cc
@@ -1,7 +1,16 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World!";
+int main(int argc, char** argv)
+{
+// Hacky way to ensure correct ssh is used, need to ensure this is where user's ssh is installed
+    std::string commandbuild = "/usr/bin/ssh";
+    for (int i = 1; i < argc - 1; ++i)
+        commandbuild = commandbuild + " " + argv[i];
+    
+    //commandbuild = commandbuild + " " + "\"cd ~/work; ./falsessh\"";
+    commandbuild = commandbuild + " " + argv[argc - 1];
+    const char *command = commandbuild.c_str();
+    system(command);
+    //cout << command;
     return 0;
 }
-
