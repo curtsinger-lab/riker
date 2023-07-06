4c49163d52667e14241f8c7620b82570c6ce7601
Zimmerea <zimmerma4@grinnell.edu>
2023-07-06 15:48:08 -0500

    Autopath ssh

--- src/ssh-wrapper/ssh-wrapper.cc
+++ src/ssh-wrapper/ssh-wrapper.cc
@@ -1,17 +1,69 @@
 #include <iostream>
+#include <optional>
+#include <string>
+#include <vector>
 
 #include <string.h>
 #include <unistd.h>
 
 #include "util/options.hh"
 
+using std::nullopt;
+using std::optional;
+using std::string;
+using std::vector;
+
+/// Keep a vector of entries in the PATH environment variable
+vector<string> path;
+
+/// Is this a wrapper around clang?
+bool is_clang = false;
+
+// STOLEN from wrapper.cc
+void init_path() {
+  std::string newpath = "";
+
+  if (char* path_str = getenv("PATH"); path_str != NULL) {
+    string old_path(path_str);
+
+    // Split the path at colon characters until no separators remain
+    size_t start = 0;
+    while (start < old_path.size()) {
+      // Look for a colon separator
+      size_t sep = old_path.find(':', start);
+
+      // If we didn't find a colon, put the separtor at the end of the string
+      if (sep == string::npos) sep = old_path.size();
+
+      // Grab the next part of the path variable
+      const auto part = old_path.substr(start, sep - start);
+
+      // If the part is not a path to the wrappers directory, include it
+      if (part.find("share/rkr/wrappers") == string::npos) {
+        // Add the part to the path vector
+        path.push_back(part);
+        if (newpath.size() > 0) newpath += ':';
+        newpath += part;
+      }
+
+      // Move past the colon separator
+      start = sep + 1;
+    }
+  }
+
+  // Update the PATH environment variable
+  setenv("PATH", newpath.c_str(), 1);
+}
+
 int main(int argc, char* argv[]) {
+  init_path();
+
   const char* remote_path = getenv("RKR_REMOTE_PATH");
 
-  char* command[argc + 1];  // save space for commands given + trace command
+  char* command[argc + 2];  // save space for commands given + trace command
 
   // TODO: Handle PATH so that we can call the user's ssh instead of slogin
-  command[0] = strdup("slogin");
+  command[0] = strdup("ssh");
   int cIndex = 1;  // index for custom arguments
   int aIndex = 1;  // index for command line arguments
   int dashCount = 0;
@@ -48,7 +100,7 @@ int main(int argc, char* argv[]) {
   command[cIndex] = (char*)NULL;
 
   // execute the command
-  execvp("slogin", command);
+  execvp("ssh", command);
 
   return 0;
 }
