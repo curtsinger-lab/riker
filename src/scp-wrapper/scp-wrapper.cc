8c8020e341d57e3e340fe4aef49d3d26523d966d
Zimmerea <zimmerma4@grinnell.edu>
2023-07-07 16:23:11 -0500

    Fix issue with pathing in scp wrapper

--- src/scp-wrapper/scp-wrapper.cc
+++ src/scp-wrapper/scp-wrapper.cc
@@ -56,17 +56,23 @@ void init_path() {
 }
 
 int main(int argc, char** argv) {
+  string full_path = getenv("PATH");
+  size_t sep = full_path.find(':', 0);
+  string riker_path = full_path.substr(0, sep);
+  printf("path: %s\n", riker_path.c_str());
+
   init_path();
-  
+
   // Can also use /usr/bin/ssh if slogin is not available
   std::string commandbuild = "scp";
 
   // Use option -S to use riker's ssh instead of scp's auto use of system ssh
-  commandbuild = commandbuild + " -S ssh";
+  if (getenv("RKR_REMOTE_PATH") != NULL) {
+    commandbuild = commandbuild + " -S " + riker_path + "/ssh";
+  }
 
   // TODO: Allow multiple command line arguments for scp
-  for (int i = 1; i < argc; ++i) 
-  	commandbuild = commandbuild + " " + argv[i];
+  for (int i = 1; i < argc; ++i) commandbuild = commandbuild + " " + argv[i];
 
   const char* command = commandbuild.c_str();
   // TODO: Switch this to an exec call with PATH handling
