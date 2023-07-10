69e27cd8ee1e780b89a865879896f99aa3c3526b
Zimmerea <zimmerma4@grinnell.edu>
2023-07-10 15:37:13 -0500

    Add ability to remote-trace to process arguments for both riker and ssh commands

--- src/wrappers/ssh-wrapper/remote-trace.cc
+++ src/wrappers/ssh-wrapper/remote-trace.cc
@@ -16,13 +16,10 @@ int main(int argc, char* argv[]) {
   // TODO: Restructure this function to trace without creating a rikerfile (w/ forking into new
   // process maybe)
 
-  // Build the commands sent to run over ssh connection
-  char* command[argc + 10];
-
-  // copy the comment to the array
-  for (int i = 1; i < argc; i++) {
-    command[i] = strdup(argv[i]);
-  }
+  // Seperate list of arguments that will be passed to rkr
+  char* argv_for_rkr[argc];
+  argv_for_rkr[0] = strdup("~/riker/debug/bin/rkr");
+  int argc_for_rkr = 1;
 
   // open the Rikerfile
   // TODO: Delete newly made Rikerfile at end. Also maybe handle if there is already a rikerfile in
@@ -36,14 +33,27 @@ int main(int argc, char* argv[]) {
 
   // write command to the Rikerfile
   outdata << "#!/bin/sh\n\n";
+  int dashCount = 0;
   for (int i = 1; i < argc; i++) {
-    outdata << command[i] << " ";
+    // Check if commandline argument is meant for riker or for program started by user
+    if (argv[i][0] == '-' && dashCount == 0) {
+      argv_for_rkr[argc_for_rkr] = strdup(argv[i]);
+      argc_for_rkr++;
+    } else {
+      outdata << argv[i] << " ";
+      dashCount++;
+    }
   }
 
   outdata.close();
 
   // TODO: ensure we are in the correct directory for riker to be called.
   // TODO: Pass along arguments from local to remote side (particularly --fresh)
-  system("~/riker/debug/bin/rkr");
+  // system("~/riker/debug/bin/rkr --fresh --show");
+  execvp("~/riker/debug/bin/rkr", argv_for_rkr);
+
+  for (int j = 0; j < argc_for_rkr; j++) {
+    free(argv_for_rkr[j]);
+  }
   return 0;
 }
