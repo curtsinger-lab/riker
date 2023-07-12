04f11229b84e8c4c3a113b4992d47d88bf061bd1
Zimmerea <zimmerma4@grinnell.edu>
2023-07-12 16:04:02 -0500

    Move init-path function into general wrapper utils header

--- src/wrappers/compiler-wrapper/compiler-wrapper.cc
+++ src/wrappers/compiler-wrapper/compiler-wrapper.cc
@@ -10,6 +10,9 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+// TODO: make path non-relative
+#include "../wrappers/wrapper-utils.hh"
+
 namespace fs = std::filesystem;
 
 using std::nullopt;
@@ -17,46 +20,8 @@ using std::optional;
 using std::string;
 using std::vector;
 
-/// Keep a vector of entries in the PATH environment variable
-vector<string> path;
-
 /// Is this a wrapper around clang?
 bool is_clang = false;
-
-void init_path() {
-  string newpath = "";
-
-  if (char* path_str = getenv("PATH"); path_str != NULL) {
-    string old_path(path_str);
-
-    // Split the path at colon characters until no separators remain
-    size_t start = 0;
-    while (start < old_path.size()) {
-      // Look for a colon separator
-      size_t sep = old_path.find(':', start);
-
-      // If we didn't find a colon, put the separtor at the end of the string
-      if (sep == string::npos) sep = old_path.size();
-
-      // Grab the next part of the path variable
-      const auto part = old_path.substr(start, sep - start);
-
-      // If the part is not a path to the wrappers directory, include it
-      if (part.find("share/rkr/wrappers") == string::npos) {
-        // Add the part to the path vector
-        path.push_back(part);
-        if (newpath.size() > 0) newpath += ':';
-        newpath += part;
-      }
-
-      // Move past the colon separator
-      start = sep + 1;
-    }
-  }
-
-  // Update the PATH environment variable
-  setenv("PATH", newpath.c_str(), 1);
-}
 
 typedef int (*execve_fn_t)(const char*, char* const*, char* const*);
 
--- src/wrappers/ssh-wrapper/ssh-wrapper.cc
+++ src/wrappers/ssh-wrapper/ssh-wrapper.cc
@@ -6,54 +6,14 @@
 #include <string.h>
 #include <unistd.h>
 
+// TODO: make path non-relative
+#include "../wrappers/wrapper-utils.hh"
 #include "util/options.hh"
 
 using std::nullopt;
 using std::optional;
 using std::string;
 using std::vector;
-
-/// Keep a vector of entries in the PATH environment variable
-vector<string> path;
-
-/// Is this a wrapper around clang?
-bool is_clang = false;
-
-// STOLEN from wrapper.cc
-void init_path() {
-  std::string newpath = "";
-
-  if (char* path_str = getenv("PATH"); path_str != NULL) {
-    string old_path(path_str);
-
-    // Split the path at colon characters until no separators remain
-    size_t start = 0;
-    while (start < old_path.size()) {
-      // Look for a colon separator
-      size_t sep = old_path.find(':', start);
-
-      // If we didn't find a colon, put the separtor at the end of the string
-      if (sep == string::npos) sep = old_path.size();
-
-      // Grab the next part of the path variable
-      const auto part = old_path.substr(start, sep - start);
-
-      // If the part is not a path to the wrappers directory, include it
-      if (part.find("share/rkr/wrappers") == string::npos) {
-        // Add the part to the path vector
-        path.push_back(part);
-        if (newpath.size() > 0) newpath += ':';
-        newpath += part;
-      }
-
-      // Move past the colon separator
-      start = sep + 1;
-    }
-  }
-
-  // Update the PATH environment variable
-  setenv("PATH", newpath.c_str(), 1);
-}
 
 int main(int argc, char* argv[]) {
   // Combine machine specific called remot
